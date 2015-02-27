8f284263cf999bf81cae7bf67a8dc71a6c6ce305
mirage335 <spamfreemirage335@gmail.com>
2015-02-27 01:02:43 +0000

    Update script.

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/linux32 bash
+#x32
+#Assuming major version changes have not broken the script, some editing will still be required. Look for x32 and x64 comments.
+
+echo $$
+
+. ubiquitous_bash.sh
+
+mustBeRoot
+
+#Execute instructions in ChRoot environment.
+executeChRoot() {
+	env -i HOME="/root" SHELL="/bin/bash" TERM="xterm" PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" $(which chroot) ./ChRoot /bin/bash -c "$@"
+}
+
+#Automatically merge proposed etc filesystem changes.
+autoEtcUpdate() {
+	executeChRoot "yes | etc-update --automode -3"
+}
+
+#####
+
+executeChRoot "emerge --sync"
+executeChRoot "emerge -vuDN --newuse --with-bdeps y --keep-going world"
+autoEtcUpdate
+executeChRoot "emerge -vuDN --newuse --with-bdeps y --keep-going world"
+
+executeChRoot "revdep-rebuild"
+#executeChRoot "perl-cleaner --reallyall" #Not usually necessary post-install, causes problems.
+executeChRoot "emerge @preserved-rebuild"
+executeChRoot "emerge -vuDN --newuse --with-bdeps y --keep-going world"
+executeChRoot "emerge @preserved-rebuild"
